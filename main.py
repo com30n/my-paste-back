4b879d388e45fb89c0615131589efd84021d6512
Evgeniy Shubin <evgeniy.shubin.android@gmail.com>
2021-08-23 22:09:19 +0300

    Fixes: Solved an issue with pre-generated permlink;
    Features:
    Added possibility for update a paste;
    Added deleted_at time for a response (time when a paste will be deleted (datetime.now() + ttl))
    Added middleware with CORS functionality

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import string
 import random
 
 import aioredis
-from typing import Optional
 
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
 
 redis = aioredis.from_url("redis://localhost:6379")
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def generate_token(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
@@ -19,18 +33,34 @@ def generate_token(size=6, chars=string.ascii_uppercase + string.ascii_lowercase
 class Paste(BaseModel):
     ttl: int = 300
     body: str
-    created_at: datetime = Field(datetime.now())
-    updated_at: datetime = Field(datetime.now())
-    permalink: str = Field(generate_token())
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    deleted_at: Optional[datetime] = None
+    permalink: Optional[str] = None
 
 
-@app.post("/p")
+@app.post("/paste/")
 async def save_paste(paste: Paste):
+    if paste.permalink:
+        loaded_paste = Paste.parse_raw(await redis.get(paste.permalink))
+        if loaded_paste:
+            loaded_paste.body = paste.body
+            now = datetime.now()
+            loaded_paste.updated_at = now
+            loaded_paste.deleted_at = now + timedelta(0, await redis.ttl(loaded_paste.permalink))
+            paste = loaded_paste
+    else:
+        paste.permalink = generate_token()
+        now = datetime.now()
+        paste.created_at = now
+        paste.updated_at = now
+        paste.deleted_at = now + timedelta(0, paste.ttl)
+
     await redis.set(paste.permalink, paste.json(), ex=paste.ttl)
-    return True
+    return paste
 
 
-@app.get("/p/{paste_name}")
+@app.get("/paste/{paste_name}")
 async def get_paste(paste_name: str) -> Paste:
     try:
         paste = Paste.parse_raw(await redis.get(paste_name))
